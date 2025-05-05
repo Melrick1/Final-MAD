46cb110b92ef594e48083e8271b12bc1f0794ff3
Melrick1 <melrickbatuwael@gmail.com>
2025-05-05 14:32:08 +0800

    perbaikan React NativeAsyncStorage

--- src/config/FirebaseAPI.jsx
+++ src/config/FirebaseAPI.jsx
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getDatabase } from "firebase/database";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
